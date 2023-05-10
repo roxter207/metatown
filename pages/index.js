df1f9e7f0dbd4fbabc1222c4174e5d9874d2a17e
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-05-10 15:29:02 +0530

    manythingshavebeenchanged

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Footer, Navbar } from "../components";
+import { Footer, Navbar } from '../components';
 import {
   About,
   Explore,
@@ -8,7 +8,7 @@ import {
   Insights,
   WhatsNew,
   World,
-} from "../sections";
+} from '../sections';
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -24,7 +24,7 @@ const Home = () => (
       <div className="gradient-04 z-0" />
       <GetStarted />
     </div>
-      <WhatsNew /> 
+    <WhatsNew />
     <div className="relative">
       <div className="gradient-04 z-0" />
       <Feedback />
